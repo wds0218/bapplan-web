ef781247153dbcca75c620b6ab8e7d86356eaaa2
wds0218 <wds0218@gmail.com>
2026-08-12 15:30:13 +0700

    밥플랜 웹앱 배포 (PWA)

---
+++ assets/web-BN-EP0Ai.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-CLtv6Ppk.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
